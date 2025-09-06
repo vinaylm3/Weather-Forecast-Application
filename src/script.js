185b5e53cbc743500620bfb3343186ebc2e0054d
Guduru Vinay <guduruvinay3@gmail.com>
2025-09-06 18:26:21 +0530

    Added 5-day forecast

--- src/script.js
+++ src/script.js
@@ -6,6 +6,7 @@ searchCityBtn.addEventListener('click', () => {
     console.log(`Searching weather for city: ${city}`);
     // Add functionality to fetch and display weather for the entered city
     fetchData(city);
+    fetchForecastData(city);
 })
 
 useCurrentLocationBtn.addEventListener('click', () => {
@@ -14,9 +15,10 @@ useCurrentLocationBtn.addEventListener('click', () => {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position) => {
             const { latitude, longitude } = position.coords;
-            console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+            // console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
             // Fetch weather data using latitude and longitude
             fetchDataByCoords(latitude, longitude);
+            fetchForecastDataByCoords(latitude, longitude);
         }, (error) => {
             console.error('Error fetching location:', error);
         });
@@ -33,6 +35,18 @@ async function fetchData(city) {
 
 async function fetchDataByCoords(lat, lon) {
     let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=cc8a426c15f687049c1626d5f6b41409&units=metric`);
+    let data = await response.json();
+    console.log(data);
+}
+
+async function fetchForecastData(city) {
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=cc8a426c15f687049c1626d5f6b41409&units=metric`);
+    let data = await response.json();
+    console.log(data);
+}
+
+async function fetchForecastDataByCoords(lat, lon) {
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=cc8a426c15f687049c1626d5f6b41409&units=metric`);
     let data = await response.json();
     console.log(data);
 }
